049635167681eae36d0916fe538dabf8c433a6d8
NamNX <0938229699a@gmail.com>
2019-05-27 22:48:01 +0700

    Update Template Source

---
+++ src/main/java/Study2019/SpringBoot_Security_JPA/SpringBootSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package Study2019.SpringBoot_Security_JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Study2019/SpringBoot_Security_JPA/SpringBootSecurityJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package Study2019.SpringBoot_Security_JPA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSecurityJpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
